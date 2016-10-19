a24ebeb1e901b33b7218173f688bbd83d899db6d
Abhishek Kc <kcabhish@gmail.com>
2016-10-18 20:41:12 -0400

    Creating objects using shorthand property in ES6

---
+++ shorthand.js
@@ -0,0 +1,12 @@
+var firstName = "Stewie";
+var lastName = "Griffin";
+
+var person = {firstName, lastName}
+
+console.log(person);
+
+var show = "Family Guy";
+
+var team = {person, show};
+
+console.log(team);
