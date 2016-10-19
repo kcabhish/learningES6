f9009df86b0db7d29072387557efc5c1cfc0452b
Abhishek Kc <kcabhish@gmail.com>
2016-10-18 20:47:53 -0400

    Added shorthand properties to add new function the new way

--- shorthand.js
+++ shorthand.js
@@ -10,3 +10,27 @@ var show = "Family Guy";
 var team = {person, show};
 
 console.log(team);
+
+var color = "red";
+var speed = 10;
+var drive = "go";
+var car = {
+    color,
+    speed,
+    [drive]: function(){
+        console.log("vroom");
+    },
+    oldway:function(){
+        console.log("This is the old way");
+    },
+    test(){
+        console.log("testing function");
+    }
+};
+
+
+
+console.log(car.color);
+car[drive]();
+car.oldway();
+car.test();
